4f92e4d2faafe5be2545e3bc53fa94e11cb922ee
Damjan Georgievski <gdamjan@gmail.com>
2008-09-10 00:19:33 +0200

    Genshi 0.5 has some different defaults about error checking.

--- vezilka/templates/__init__.py
+++ vezilka/templates/__init__.py
@@ -4,7 +4,7 @@ from genshi.template import TemplateLoader
 import genshi
 
 search_path = path.dirname(__file__)
-loader = TemplateLoader([search_path], auto_reload=True)
+loader = TemplateLoader([search_path], auto_reload=True, variable_lookup="lenient")
 
 def render(name, **context):
     suffix = path.splitext(name)[1]
@@ -14,8 +14,8 @@ def render(name, **context):
 def render_genshi(name, **context):
     tmpl = loader.load(name)
     stream = tmpl.generate(HTML=genshi.HTML, **context)
-    return stream.render('html', doctype='html', encoding=None)
-    return stream.serialize('html', doctype='html')
+    return stream.render('html', doctype='html', encoding=None) # string
+    return stream.serialize('html', doctype='html') # generator
 
 RENDER_ENGINES = {
  '.html': render_genshi,
