99884fd90b98da15fe94fc06365060eb422de785
Damjan Georgievski <gdamjan@gmail.com>
2009-07-19 00:40:21 +0200

    Vezilka: improve make_app as it needs to be (run-simple is a demo deployment)

--- run-simple.py
+++ run-simple.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python
 from os import path
 
-from werkzeug import run_simple, SharedDataMiddleware
 
 import vezilka
-import vezilka.views
-
-STATIC_PATH = path.join(path.dirname(__file__), "static")
-
 app = vezilka.make_app(db_url="http://localhost:5984/vezilka")
-app.publish(vezilka.views)
+
+
 
 if __name__ == '__main__':
+    from werkzeug import run_simple, SharedDataMiddleware
+    STATIC_PATH = path.join(path.dirname(__file__), "static")
+
     app = SharedDataMiddleware(app, {'/static':  STATIC_PATH})
     run_simple('', 5000, app, use_debugger=True, use_reloader=True)
--- vezilka/__init__.py
+++ vezilka/__init__.py
@@ -2,11 +2,14 @@ from __future__ import absolute_import, division
 
 from .lib import RESTzeug
 from .model import Database
+from . import views
 
 def make_app(config=None, **kwargs):
     config = config or {}
     config.update(kwargs)
     config['db'] = Database(config['db_url'])
-    application = RESTzeug(config)
+
+    application = RESTzeug(config=config)
+    application.publish(views)
     return application
 
